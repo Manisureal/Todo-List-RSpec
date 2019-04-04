81665aadda791b5c57f7327c0dbbdd0c562ef907
Manisureal <mansoor.zaman@sage.com>
2019-04-04 17:22:05 +0100

    further test code added to todo_list_spec file

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe TodoList, type: :model do
-  context "create a todo list" do
-  	it "todo list must have a title" do
-  		expect(subject.title).to be_nil
-  		expect(subject.save).to be false
+  describe "Create a Todo List" do
+  	
+  	context "todo list must have a title" do
+  		it "passes if title is given" do
+	  		subject.title = "Title"
+	  		allow(subject).to receive(:save).and_return(true) # stub it as it passes and we dont want to write to db
+	  		expect(subject.save).to be true
+  		end
+
+  		it "fails if title is blank" do
+  			# no need to stub it its failing and by default it wont save to db
+	  		expect(subject.title).to be_nil
+	  		expect(subject.save).to be false
+  		end
   	end
   end
 end
