6b97f512040dca25e9ef34d5d0d1813e15a45726
Manisureal <mansoor.zaman@sage.com>
2019-04-05 16:53:21 +0100

    written tests for todo list items model

--- app/models/todo_list_item.rb
+++ app/models/todo_list_item.rb
@@ -1,3 +1,4 @@
 class TodoListItem < ApplicationRecord
   belongs_to :todo_list
+  validates :description, presence: true
 end
--- spec/models/todo_list_item_spec.rb
+++ spec/models/todo_list_item_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe TodoListItem, type: :model do
-	describe "Todo List Items Associations" do
-  		it { is_expected.to belong_to :todo_list }
+	describe "Add Todo List Item to Todo List" do
+		context "Todo List Items Associations" do
+	  		it { is_expected.to belong_to :todo_list }
+		end
+
+		context "Todo List Items Validations" do
+			it { is_expected.to validate_presence_of :description }
+		end
+
+		context "Todo List Item completed default value" do
+			it "should be false by default" do
+				expect(subject.completed).to be false
+			end
+		end
+
 	end
 end
