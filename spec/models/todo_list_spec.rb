2158d9a55034f39be424fbac18405c6f2d6503a4
Manisureal <mansoor.zaman@sage.com>
2019-04-05 12:03:17 +0100

    Added test code for instance method improve_title_text

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -6,14 +6,24 @@ RSpec.describe TodoList, type: :model do
   	context "todo list must have a title" do
   		it "passes if title is given" do
 	  		subject.title = "Title"
-	  		allow(subject).to receive(:save).and_return(true) # stub it as it passes and we dont want to write to db
-	  		expect(subject.save).to be true
+	  		# allow(subject).to receive(:save).and_return(true) # stub it as it passes and we dont want to write to db
+	  		expect(subject.valid?).to be true
   		end
 
   		it "fails if title is blank" do
   			# no need to stub it its failing and by default it wont save to db
 	  		expect(subject.title).to be_nil
-	  		expect(subject.save).to be false
+	  		expect(subject.valid?).to be false
+  		end
+  	end
+
+  	context "#improve_title_text" do
+  		it "converts a todolist title by capitalizing first letter of every word" do
+  			todo_list = TodoList.new title: "weekend shopping list"
+  			# allow(todo_list.title).to receive(:blank?).and_return(false)
+  			# allow(todo_list).to receive(:valid?).and_return(true)
+  			# p todo_list.blank?
+  			expect(todo_list.improve_title_text).to eq("Weekend Shopping List")
   		end
   	end
   end
