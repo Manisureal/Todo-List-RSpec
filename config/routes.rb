43d0cf8577d38556af1d3efdfcc93804ce025524
Manisureal <mansoor.zaman@sage.com>
2019-04-16 15:24:05 +0100

    index view created for todo list

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get 'todo_lists/new', to: 'todo_lists#new'
   post 'todo_lists', to: 'todo_lists#create'
   get '/todo_lists/:id', to: 'todo_lists#show', as: 'todo_list'
-  get '/todo_lists/:id/edit', to: 'todo_lists#edit'
-  patch '/todo_lists/update', to: 'todo_lists#update'
-  delete 'todo_lists/:id', to: 'todo_lists#destroy'
+  get '/todo_lists/:id/edit', to: 'todo_lists#edit', as: 'edit_todo_list'
+  patch '/todo_lists/update', to: 'todo_lists#update', as: 'update_todo_list'
+  delete 'todo_lists/:id', to: 'todo_lists#destroy', as: 'delete_todo_list'
 
   get 'todo_list_items/new', to: 'todo_list_items#new'
   post 'todo_list_items', to: 'todo_list_items#create'
