37b1f6b6fb7fcdb5a4b7957dca3d3d1b3d38b4b7
Manisureal <mansoor.zaman@sage.com>
2019-04-16 14:32:57 +0100

    enabled web console for dockerized app in google chrome

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # To enable web console for a dockerized app on google chrome 
+  config.web_console.whitelisted_ips = Socket.ip_address_list.reduce([]) do |res, addrinfo|
+    addrinfo.ipv4? ? res << IPAddr.new(addrinfo.ip_address).mask(24) : res
+  end
 end
