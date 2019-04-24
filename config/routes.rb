1cdb2bfd3bd5f0e5739b96f8b82d0f3c1ca1c53f
manisureal <mansoor.zaman@sage.com>
2019-04-24 09:54:29 +0100

    created edit form for todolist items edit

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
   patch '/todo_lists/update', to: 'todo_lists#update', as: 'update_todo_list'
   delete 'todo_lists/:id', to: 'todo_lists#destroy', as: 'delete_todo_list'
 
-  get 'todo_list_items/new', to: 'todo_list_items#new'
-  post 'todo_list_items', to: 'todo_list_items#create'
-  get 'todo_list_items/:id', to: 'todo_list_items#show', as: 'show_todolist_items'
-  get 'todo_list_items/edit', to: 'todo_list_items#edit'
-  patch 'todo_list_items/:id', to: 'todo_list_items#update'
-  delete 'todo_list_items/:id', to: 'todo_list_items#destroy', as: 'delete_todolist_items'
-
+  post '/todo_list_items', to: 'todo_list_items#create'
+  get  '/todo_list_items/new', to: 'todo_list_items#new', as: 'new_todo_list_item'
+  get    '/todo_list_items/:id/edit', to: 'todo_list_items#edit', as: 'edit_todo_list_item'
+  get    '/todo_list_items/:id', to: 'todo_list_items#show', as: 'todo_list_item'
+  patch  '/todo_list_items/:id', to: 'todo_list_items#update'
+  put    '/todo_list_items/:id', to: 'todo_list_items#update'
+  delete '/todo_list_items/:id', to: 'todo_list_items#destroy'
 end
