fdce1a3d6f05368164a687174b54829f719c0629
manisureal <mansoor.zaman@sage.com>
2019-05-06 18:11:43 +0100

    improved the display for errors helper method

--- app/helpers/todo_lists_helper.rb
+++ app/helpers/todo_lists_helper.rb
@@ -1,9 +1,13 @@
 module TodoListsHelper
 	def display_form_errors(errors)
-		if errors.count > 1
-			errors[0..-2].join() + ", " + errors[-1]
+		if errors.class === Array
+			if errors.count > 1
+				errors[0..-2].join() + ", " + errors[-1]
+			else
+				errors.pop if errors.count == 1
+			end
 		else
-			errors.pop if errors.count == 1
+			errors
 		end
 	end
 end
